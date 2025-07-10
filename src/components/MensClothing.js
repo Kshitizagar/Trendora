2861d53c412dc00771fb5808d35bb202f274a217
Kshitizagar <53623530+Kshitizagar@users.noreply.github.com>
2025-07-11 02:45:21 +0530

    Update MensClothing.js

--- src/components/MensClothing.js
+++ src/components/MensClothing.js
@@ -10,17 +10,18 @@ function MensClothing({ products }) {
       <div className="product-grid">
         {mensProducts.map((product, index) => (
           <div className="product-box" key={product._id}>
-            <div className="id-category">
+            {/* <div className="id-category">
               <p><strong>ID:</strong> {index + 1}</p>
               <p>{product.category}</p>
-            </div>
+            </div> */}
             <img
               src={product.image_link}
               alt={product.name}
               className="product-image"
             />
             <p>{product.description.split(" ").slice(0, 7).join(" ")}...</p>
-            <p>Price: ₹{product.price}</p>
+            {/* <p>Price: ₹{product.price}</p> */}
+            <p style={{ fontSize: "1em", fontWeight: "600", margin: "0" }}>₹{product.price}</p>
             <button
               className="buy-button"
               onClick={() => window.open(product.product_link, '_blank')}
